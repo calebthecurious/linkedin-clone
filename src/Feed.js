70b8e03759ecc4a4e0111d60d3efb3e1be7274dd
calebthecurious <calebthecurious@gmail.com>
2022-05-16 20:04:39 +1000

    4th Commit - Post Section

--- src/Feed.js
+++ src/Feed.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Feed.css";
 import CreateIcon from '@material-ui/icons/Create';
 import InputOption from './InputOption';
@@ -6,17 +6,24 @@ import ImageIcon from '@material-ui/icons/Image';
 import SubscriptionIcon from '@material-ui/icons/Subscriptions';
 import EventNoteIcon from '@material-ui/icons/EventNote';
 import CalendarViewDayIcon from '@material-ui/icons/CalendarViewDay';
+import Post from './Post';
 
 
 
 function Feed() {
+  const [posts, setPosts] = useState([])
+
+  const sendPost = e => {
+    e.preventDefault();
+  }
+
   return <div className='feed'>
     <div className='feed__inputContainer'>
       <div className='feed__input'>
         <CreateIcon />
         <form>
           <input type="text" />
-          <button type="submit">Send</button>
+          <button onClick={sendPost} type="submit">Send</button>
         </form>
       </div>
       <div className="feed__inputOptions">
@@ -26,6 +33,16 @@ function Feed() {
         <InputOption Icon={CalendarViewDayIcon} title="Write Article" color="#7FC15E" />
       </div>
     </div>
+
+    {/* Posts */}
+    {posts.map((post) => (
+      <Post />
+    ))}
+    <Post
+      name='Caleb tc'
+      description='This is a test'
+      message='WOW this worked'
+    />
   </div>
 
 }
